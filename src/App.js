1592027fcdc280f7c2d19fd1e1067dc1f4424a62
Yaron Malin <yaron1m@gmail.com>
2018-08-25 23:14:50 +0300

    Add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import './App.css';
 import HomePageContainer from "./pages/home-page/HomePageContainer";
+import Header from "./Header";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div style={{textAlign: "center"}}>
+                <Header/>
+
                 <HomePageContainer/>
             </div>
         );
--- src/pages/home-page/HomePage.js
+++ src/pages/home-page/HomePage.js
@@ -1,11 +1,16 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-export default (props) => {
+export default function HomePage(props) {
     const title = props.name ? "Welcome " + props.name : "What is you name";
 
     return (
-        <div>
+        <div style={{fontSize: 16}}>
             {title}
         </div>
     )
 }
+
+HomePage.propTypes = {
+    name: PropTypes.string,
+};
