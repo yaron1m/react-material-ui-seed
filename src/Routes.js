87e3152297cc5cbada1cb4ef88281a9d23d5a7af
Yaron Malin <yaron1m@gmail.com>
2018-09-01 01:22:46 +0300

    Add page deployment

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {HashRouter as Router, Route} from "react-router-dom";
 import HomePageContainer from "./pages/home-page/HomePageContainer";
 
 export default function Routes() {
